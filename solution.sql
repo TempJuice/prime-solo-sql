d53f9c5f895e2599f3145bee7ac10acb74ff8e5d
Ryan Templin <tempjuice@hotmail.com>
2017-08-08 14:43:28 -0500

    Updated post lecture.

--- solution.sql
+++ solution.sql
@@ -21,23 +21,58 @@ INSERT INTO syntax_practice (username, city, transactions_completed, transaction
 SELECT * FROM syntax_practice;
 
 -- Get all users from Chicago
-SELECT * FROM syntax_practice WHERE city='chicago';
+SELECT * FROM syntax_practice WHERE city ILIKE 'chicago';
+--^ This is if we don't care about case of the letters
+-- We can specify the case also with lower('chicago')
 
 -- Get all users with usernames that contain the letter a
-SELECT * FROM syntax_practice WHERE username LIKE '%a%';
+SELECT * FROM syntax_practice WHERE username ILIKE '%a%';
+--LIKE is a problem if someone's name starts with 'a'
 
 -- The bank is giving a new customer bonus! Update all records with an account balance of 0.00 and a transactions_attempted of 0. Give them a new account balance of 10.00.
-SELECT * FROM syntax_practice WHERE account_balance = '0' AND transactions_attempted = '0';
-UPDATE syntax_practice SET account_balance = '10' WHERE user_id='25';
+UPDATE syntax_practice syntax_practice -- SELECT FROM first, so we know we have the right item
+SET account_balance = 10
+WHERE account_balance = 0 
+AND transactions_attempted = 0;
 
 -- Select all users that have attempted 9 or more transactions.
-SELECT * FROM syntax_practice WHERE transactions_attempted >= '9';
+SELECT * 
+FROM syntax_practice 
+WHERE transactions_attempted >= 9;
 
 -- Get the username and account balance of the 3 users with the highest balances, sort highest to lowest balance. NOTE: Research LIMIT
-SELECT * FROM syntax_practice ORDER BY account_balance DESC LIMIT 3; 
+SELECT * 
+FROM syntax_practice 
+ORDER BY account_balance DESC 
+LIMIT 3; 
 
 -- Get the username and account balance of the 3 users with the lowest balances, sort lowest to highest balance.
-SELECT * FROM syntax_practice ORDER BY account_balance ASC LIMIT 3;
+SELECT * 
+FROM syntax_practice 
+ORDER BY account_balance ASC 
+LIMIT 3;
+
+-- Get all users with account balances that are more than $100.
+SELECT *
+FROM syntax_practice
+WHERE account_balance > 100;
+
+-- Add a new record.
+INSERT INTO syntax_practice (username, city, transactions_completed, transactions_attempted, account_balance)
+VALUES ('peter', 'minneapolis', 400, 500, 231)
+
+-- The bank is losing money in Miami and Phoenix and needs to unload low transaction customers: Delete users that reside in miami OR phoenix and have completed fewer than 5 transactions.
+SELECT *
+FROM syntax_practice
+WHERE transactions_completed < 5
+AND (city = 'miami'
+OR city = 'phoenix');
+
+-- Another way to do it
+SELECT *
+FROM syntax_practice
+WHERE transactions_completed < 5
+AND (city = 'miami', 'phoenix');
 
 
 
