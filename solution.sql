22828a1daef95da6ad3453d7badba685626b3640
Ryan Templin <tempjuice@hotmail.com>
2017-08-08 14:08:02 -0500

    Stopping point for partner exercise.

--- solution.sql
+++ solution.sql
@@ -18,5 +18,30 @@ INSERT INTO syntax_practice (username, city, transactions_completed, transaction
 ('grace', 'miami', 7, 9100, 34.78),
 ('hope', 'phoenix', 4, 10, 50.17);
 
-SELECT * FROM syntax_practice
+SELECT * FROM syntax_practice;
+
+-- Get all users from Chicago
+SELECT * FROM syntax_practice WHERE city='chicago';
+
+-- Get all users with usernames that contain the letter a
+SELECT * FROM syntax_practice WHERE username LIKE '%a%';
+
+-- The bank is giving a new customer bonus! Update all records with an account balance of 0.00 and a transactions_attempted of 0. Give them a new account balance of 10.00.
+SELECT * FROM syntax_practice WHERE account_balance = '0' AND transactions_attempted = '0';
+UPDATE syntax_practice SET account_balance = '10' WHERE user_id='25';
+
+-- Select all users that have attempted 9 or more transactions.
+SELECT * FROM syntax_practice WHERE transactions_attempted >= '9';
+
+-- Get the username and account balance of the 3 users with the highest balances, sort highest to lowest balance. NOTE: Research LIMIT
+SELECT * FROM syntax_practice ORDER BY account_balance DESC LIMIT 3; 
+
+-- Get the username and account balance of the 3 users with the lowest balances, sort lowest to highest balance.
+SELECT * FROM syntax_practice ORDER BY account_balance ASC LIMIT 3;
+
+
+
+ 
+
+
 
